fa1df3db5ae9792c18ff29ed01de96f91975b565
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2022-10-04 21:09:31 +0530

    Line comparison Problem

---
+++ LineComparisonProblem.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day8.UC_LineComparison_OOPS;
+
+import java.util.*;
+public class LineComparisonProblem {
+
+	public static void main(String[] args) {
+		System.out.println("-------------Welcome to Line Comparison Computation Program-------------");
+
+	}
+
+}
